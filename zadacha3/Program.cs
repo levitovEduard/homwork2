1883e5987920a81dfd2d3ca11fce121f58ebf01a
Eduard <levitov.eduard@gmail.com>
2022-12-12 19:54:33 +0200

    zadacha 3

---
+++ zadacha3/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+int num = Convert.ToInt32(Console.ReadLine());
+            int index = 1;
+            while(index <= num)
+            {
+                Console.Write(index * index * index);
+                Console.Write(" ");
+                index += 1;
+            }
